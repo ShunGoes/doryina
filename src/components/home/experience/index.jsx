ac9ea4dc115a7cc5c3a1afd358c2f0459a36e03c
shunGoes <shunnode@gmail.com>
2024-09-05 08:33:03 +0100

    used prototype

--- src/components/home/experience/index.jsx
+++ src/components/home/experience/index.jsx
@@ -4,14 +4,14 @@ import helper from "../../../helper";
 const Experience = () => {
   return (
     <div className="bg-[#F9F2F8] py-[60px]">
-      <div className="md:flex md:px-[60px] ">
-        <div className="md:w-[70%] ">
+      <div className="md:flex md:w-11/12 md:mx-auto  md:h-[80vh]">
+        <div className="md:w-[50%]  md:flex flex-col justify-center">
           <h2 className="heading-2 md:text-start mb-3">Our Experience </h2>
-          <p className="heading-2-paragragh md:text-start w-11/12 mx-auto md:mx-0 md:w-[65%] md:pr-7">
+          <p className="heading-2-paragragh md:text-start w-11/12 mx-auto md:mx-0 md:w-[78%] ">
             since inception we have always prioritize the convenience of our
             clients by providing low prices and easy processes
           </p>
-          <div className="grid grid-cols-3 place-items-center justify-between gap-[20px] w-10/12 md:w-[45%] md:mx-0 mx-auto   mt-[50px]">
+          <div className="grid grid-cols-3 place-items-center justify-between gap-[20px] w-10/12 md:w-[60%] md:mx-0 mx-auto   mt-[50px]">
             <div className="h-[90px] ">
               <h4 className="font-[700] text-[32px] leading-[35.2px] text-[#962985] mb-3">
                 10
@@ -38,13 +38,13 @@ const Experience = () => {
             </div>
           </div>
           <div className="flex justify-center md:justify-start">
-            <button className="btn-colored mt-[40px] w-4/12 md:w-2/12">
+            <button className="btn-colored mt-[40px] w-4/12 md:w-[20%]">
               Get started{" "}
             </button>
           </div>
         </div>
         {/* parallax image */}
-        <div className="mt-[60px] md:my-0 mb-[30px] flex justify-center  ">
+        <div className="mt-[60px] md:my-0 mb-[30px]  md:w-[50%] flex justify-center  ">
           <img src={helper.ParallaxGirl} alt="girl smiling" className=""/>
         </div>
       </div>
