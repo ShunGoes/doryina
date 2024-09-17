ce99e5e8c2543b9607a3b8a14e094450a7520c9b
shunGoes <shunnode@gmail.com>
2024-09-17 10:19:34 +0100

    adding webp images to website

--- src/components/accordion-comp/index.jsx
+++ src/components/accordion-comp/index.jsx
@@ -5,10 +5,16 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "../ui/accordion";
+import { motion } from "framer-motion";
 
 const CustomAccordion = ({ question, children }) => {
   return (
-    <div className="bg-[#300D2B] rounded-[6px] ">
+    <motion.div
+      initial={{ opacity: 0, y:100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, type: "spring", bounce: 0.6 }}
+      viewport={{ once: true }}
+      className="bg-[#300D2B] rounded-[6px] ">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
           <AccordionTrigger className="text-[14px] h-[70px] font-[400] leading-[18px] text-white px-[10px] text-start md:text-[22px] md:leading-[18px] md:px-[30px]">
@@ -17,7 +23,7 @@ const CustomAccordion = ({ question, children }) => {
           <AccordionContent>{children}</AccordionContent>
         </AccordionItem>
       </Accordion>
-    </div>
+    </motion.div>
   );
 };
 
